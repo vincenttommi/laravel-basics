ee93762834df32dc59ce0e53974fc1132e00a20c
vincenttommi <vincenttommi@gmail.com>
2022-09-07 00:54:32 +0300

    routing and response in laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+
+   return  response ('<h1>hello world</h1>', 404);
+    
+
+});
