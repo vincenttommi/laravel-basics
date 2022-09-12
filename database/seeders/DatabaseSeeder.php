9c995c4546ab46121896819dd65cd430cf2cb3cb
vincenttommi <vincenttommi@gmail.com>
2022-09-13 02:46:20 +0300

    creating factory in laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,33 +17,34 @@ class DatabaseSeeder extends Seeder
     {
         
         \App\Models\User::factory(5)->create();
+        Listing::factory(6)->create();
 
-        Listing::create([
+    //     Listing::create([
 
 
-          'title' => 'laravel senior developer',
-           'tags' =>  'laravel, javascript',
-           'company' => 'Blocktech kenya',
-           'location' => 'Nairobi,Boston',
-           'email' => 'blocktech@gmail.com', 
-            'website' => 'http://www.blocktech.com',
-           'description' => 'we make scalable products',
+    //       'title' => 'laravel senior developer',
+    //        'tags' =>  'laravel, javascript',
+    //        'company' => 'Blocktech kenya',
+    //        'location' => 'Nairobi,Boston',
+    //        'email' => 'blocktech@gmail.com', 
+    //         'website' => 'http://www.blocktech.com',
+    //        'description' => 'we make scalable products',
            
 
 
 
 
-        ]);
+    //     ]);
     
 
-     Listing::create([
-        'title' => 'Fullstack engineer',
-        'tags' => 'laravel,backend, api,',
-        'company' => 'stack industries',
-        'location' => 'Newyork,NY',
-        'email'=> 'fullstack@gmail.com',
-        'website' => 'http://www.stackindustries.com',
-        'description' => 'Modern bootcamp company',
-     ]);
+    //  Listing::create([
+    //     'title' => 'Fullstack engineer',
+    //     'tags' => 'laravel,backend, api,',
+    //     'company' => 'stack industries',
+    //     'location' => 'Newyork,NY',
+    //     'email'=> 'fullstack@gmail.com',
+    //     'website' => 'http://www.stackindustries.com',
+    //     'description' => 'Modern bootcamp company',
+    //  ]);
     }
 }
