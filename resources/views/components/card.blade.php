38f33c21b5d51d72d927eabe692a5ac4026e4075
vincenttommi <vincenttommi@gmail.com>
2022-09-19 09:00:20 +0300

    blade components in laravel

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+
+    {{$slot}}
+    {{-- outputs the tags whenever we pass information --}}
+
+</div>   
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,9 +7,9 @@
 <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back </a>
 
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-24">
 <div  class="flex flex-col items-center justify-center text-center" >
-            <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image')}}" alt="" />
+            <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="" />
 
             <h3 class="text-2xl mb-2">{{$listing->title}}r</h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
@@ -64,7 +64,7 @@
                 </div>
             </div>
         </div>
-    </div>
+</x-card>
 </div>
 
 
